49a4b193481d3cb06e811104fa760360298083de
EpicWin332 <gamzatovmagomed@mail.ru>
2014-10-09 20:13:52 +0400

    add maim menu

---
+++ ProjectX/Assets/Scripts/StartScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+
+	public Texture2D buttonTexture;
+
+	void OnGUI(){
+
+		if (GUI.Button 
+		    (new Rect (camera.pixelWidth / 2 - buttonTexture.width/2, camera.pixelHeight / 2 - buttonTexture.height/2 
+		           ,buttonTexture.width, buttonTexture.height), buttonTexture, GUIStyle.none)) 
+		   
+		{
+			Application.LoadLevel(1);
+			return;
+				}
+
+	}
+}
