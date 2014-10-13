ae0c4696f17eecb32eb65bec5ffefa430f436bb0
AlbertPodusenko <albert.podusenko@gmail.com>
2014-10-14 03:13:08 +0400

    noPhysicxOnlyComents

--- ProjectX/Assets/Scripts/SpawnScript.cs
+++ ProjectX/Assets/Scripts/SpawnScript.cs
@@ -7,6 +7,10 @@ public class SpawnScript : MonoBehaviour
 	public float SpawnMin =1f;
 	public float SpawnMax = 1.5f;
 	public float x,y;
+	public float a=600f;
+	public float tao=0.0223f;
+	public float g1,g2;
+	public int switchcase=2;
 	public bool counter=true;
 	// Use this for initialization
 	void Start () 
@@ -14,21 +18,36 @@ public class SpawnScript : MonoBehaviour
 		//начальное положение генерируемой платформы
 		x = 2.5f;
 		y = -1f;
+		g1 = a * a * tao * tao / (-60);
+		g2 = 2 * 5*(600+30)*tao/30;
 		Spawn ();
 	}
 	void Spawn()
 	{
-
-		Instantiate (obj [1], new Vector2(x,y)/*transform.position*/, Quaternion.identity);
+		//rigidbody2D.position = new Vector2 (2, -1);
 		System.Random rnd = new System.Random ();
-		Invoke ("Spawn", Random.Range (SpawnMin,SpawnMax));
+		switch(switchcase)
+		{
+			case 1:
+				Instantiate (obj [1], new Vector2(0.38f,-1f), Quaternion.identity);//учесть положение пацана
+				switchcase=1;
+				break;
+			case 2:
+				//Instantiate (obj [1], new Vector2(-100f,-100f), Quaternion.identity);
+				switchcase=1;
+				break;
+			default:
+			break;
+		}
+			
+			Invoke ("Spawn", Random.Range (SpawnMin,SpawnMax));
 		//если количество spawn элементов становится большим, то нужно прекратить генерацию
 		//нужно генерировать такие платформы, дельта, которых по x и y была бы от [
-		if (rnd.Next (0, 2) == 1)
+		/*if (rnd.Next (0, 2) == 1)
 						x = x + 1f;
 				else
 						x = x + 2f;
-		if ((y<=-5) || counter)
+		if ((y<=-4) || counter)
 		{
 			y =y+2;
 			counter=false;
@@ -37,7 +56,7 @@ public class SpawnScript : MonoBehaviour
 		{
 			y =y-rnd.Next (2,4);
 			counter=true;
-		}
+		}*/
 	}
 
 }
