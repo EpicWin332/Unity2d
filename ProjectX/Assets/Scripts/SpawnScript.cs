cc2f6eceb121e18bc4cb3fab9d579a60f6cad6f0
AlbertPodusenko <albert.podusenko@gmail.com>
2014-10-18 20:42:03 +0400

    хуйня

--- ProjectX/Assets/Scripts/SpawnScript.cs
+++ ProjectX/Assets/Scripts/SpawnScript.cs
@@ -5,7 +5,7 @@ public class SpawnScript : MonoBehaviour
 {
 		public GameObject[] obj;
 		public GameObject startPlatform, clone;
-		public float y0 = -0.7f, y = 1f, t1,x0=0,x; //squareEquation
+		public float y0 = -0.7f, y = 1f, t1, x0 = 0, x; //squareEquation
 		public int switchcase = 1;
 		public bool counter = true;
 		bool flag = true;
@@ -13,8 +13,8 @@ public class SpawnScript : MonoBehaviour
 		float SquareEquation (float y0, float y)
 		{
 				float t2;
-				t1 = (-12f + Mathf.Sqrt (12f * 12f + 4f * 15f * (y0 - y))) / (-30f);
-				t2 = (-12f - Mathf.Sqrt (12f * 12f + 4f * 15f * (y0 - y))) / (-30f);
+				t1 = (-12f + Mathf.Sqrt (12f * 12f + 4f * 15f * (y0 - y-0.12f))) / (-30f);
+				t2 = (-12f - Mathf.Sqrt (12f * 12f + 4f * 15f * (y0 - y-0.12f))) / (-30f);
 				if (t1 > t2)
 						return t1;
 				else {
@@ -35,8 +35,8 @@ public class SpawnScript : MonoBehaviour
 							y = Random.Range (-1F, 5F);
 				//y0 = 1.5f;
 					}*/
-		t1 = SquareEquation (y0, y);
-		//x = x0 + 5f * t1;
+				t1 = SquareEquation (y0, y);
+				//x = x0 + 5f * t1;
 		}
 
 		void Update ()
@@ -46,33 +46,32 @@ public class SpawnScript : MonoBehaviour
 						flag = false;
 				}*/
 				//rigidbody2D.position = new Vector2 (2, -1);
-			if (flag)	
+				if (flag)	
+			    
+				if (startPlatform.rigidbody2D.position.x < 0f) {
+						t1 = SquareEquation (y0, y);
+						clone = Instantiate (obj [2], new Vector2 (x + 5f * t1, y - 0.4f), Quaternion.identity) as GameObject;
+						x = x + 5f;
+						y0 = y - 0.3f;//character.rigidbody2D.position.y-0.3f;//берет координаты в середине человека поэтому -0.3
+						y = Random.Range (-1f, 5f); 
+						while (y-y0>2.3F)//область в которой функция будет иметь вещественные корни
+								y = Random.Range (-1F, 5F);
+						flag = false;
+				y0 = 0f;
+				}
 
-		    
-					if (startPlatform.rigidbody2D.position.x<-1f)
-								
-									{
-										t1 = SquareEquation (y0, y);
-										clone=Instantiate (obj [2], new Vector2 (x+5f * t1, y - 0.4f), Quaternion.identity) as GameObject;
-										x=x+5f;
-										y0 = y - 0.3f;//character.rigidbody2D.position.y-0.3f;//берет координаты в середине человека поэтому -0.3
-										y = Random.Range (-1f, 5f); 
-										while (y-y0>2.3F)//область в которой функция будет иметь вещественные корни
-											y = Random.Range (-1F, 5F);
-										flag=false;
-									}
 					
-							if (!flag)
-								{
-									t1 = SquareEquation (y0, y);
-									if (clone.rigidbody2D.position.x < 0F)
-											clone = Instantiate (obj [2], new Vector2 (5f * t1, y - 0.7f), Quaternion.identity) as GameObject;//ставит платформу относительно её середины
-									y0 = y - 0.3f;//character.rigidbody2D.position.y-0.3f;//берет координаты в середине человека поэтому -0.3
-									y = Random.Range (-1f, 5f); 
-									while (y-y0>2.3F)//область в которой функция будет иметь вещественные корни
-											y = Random.Range (-1F, 5F);
-								}
-								//print(t1);//учесть положение пацана
+				if (!flag) {
+						t1 = SquareEquation (y0, y-0.8f);
+						if (Input.GetButtonDown ("Fire1"))//ГЕНЕРИРУЕМ ДО ТОГО КАК ОН БУДЕТ В УКАЗАННОЙ ТОЧКЕ 
+						clone = Instantiate (obj [2], new Vector2 (5f * t1, y-0.8f), Quaternion.identity) as GameObject; //ставит платформу относительно её середины
+						y0 = y-0.8f;//ОШИБКА!!!//character.rigidbody2D.position.y-0.3f;//берет координаты в середине человека поэтому -0.3
+						y = Random.Range (-1f, 5f); 
+							while (y-y0>2.3F)//область в которой функция будет иметь вещественные корни
+									y = Random.Range (-1F, 5F);
+						//y = Random.Range (y0-2.2f, y0+2f); 
+				}
+				//print(t1);//учесть положение пацана
 						
 					
 
