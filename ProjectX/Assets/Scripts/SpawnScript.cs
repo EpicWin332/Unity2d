d1ad5b0e591058444f2072c0a3666877b5ed613c
AlbertPodusenko <albert.podusenko@gmail.com>
2014-10-21 16:13:09 +0400

    более менее
    нужно исправить бесконечный цикл

--- ProjectX/Assets/Scripts/SpawnScript.cs
+++ ProjectX/Assets/Scripts/SpawnScript.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class SpawnScript : MonoBehaviour
 {
 		public GameObject[] obj;
-		public GameObject startPlatform, clone,clone1;
-		public float y0 = -0.4f,sY, firstY = 1f,secondY=-2f, t1, x0 = 0, x, funRecord; //squareEquation
-		public int switchcase = 1, next;
+		public GameObject startPlatform, clone, clone1;
+		public float y0 = -0.4f, sY, firstY = 1f, secondY = -2f, t1, x0 = 0, x, funRecord; //squareEquation
+		public int switchcase = 1, next, prev;
+		int[] values = new int[5] {0,0,1,1,2};
 		public bool counter = true;
 		bool flag = true;
 
@@ -38,7 +39,7 @@ public class SpawnScript : MonoBehaviour
 	
 		void Start ()
 		{
-				sY=y0;
+				sY = y0;
 				if (Random.Range (0, 1) == 1) {
 						t1 = SquareEquationBig (y0, firstY);
 						funRecord = 5F;
@@ -55,13 +56,16 @@ public class SpawnScript : MonoBehaviour
 						flag = false;
 				}*/
 				//rigidbody2D.position = new Vector2 (2, -1);
+				System.Random rnd = new System.Random ();
 				if (flag)	
 			    
 				if (startPlatform.transform.position.x < 3.5f) {
 						t1 = SquareEquationSmall (y0, firstY);
-						clone = Instantiate (obj [0], new Vector2 (3.478378f * 2f + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
-						clone1 = Instantiate (obj [0], new Vector2 (4 * 2f + 5f * t1, secondY - 0.8f), Quaternion.identity) as GameObject;
-						
+						next = rnd.Next (0, values.Length);
+						next = values [next];
+						clone = Instantiate (obj [next], new Vector2 (3.478378f * 2f + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
+						//clone1 = Instantiate (obj [0], new Vector2 (4 * 2f + 5f * t1, secondY - 0.8f), Quaternion.identity) as GameObject;
+						prev = next;
 						y0 = firstY - 0.8f;//character.rigidbody2D.position.y-0.3f;//берет координаты в середине человека поэтому -0.3
 						firstY = Random.Range (-1f, 5f); 
 						while (firstY-y0>2.3F)//область в которой функция будет иметь вещественные корни
@@ -75,35 +79,78 @@ public class SpawnScript : MonoBehaviour
 								t1 = SquareEquationBig (y0, firstY);
 						else
 								t1 = SquareEquationSmall (y0, firstY);
-						if (float.IsNaN(t1))
-							{
-								t1=0.3f; //костыль
-							}
-			//ЗАВИСИМОСТЬ НУЖНО ДЕЛАТЬ ОТ ПРЕДЫДУЩЕЙ ПЛАТФОРМЫ!			
-			if (clone.rigidbody2D.position.x < 4f) {			//ГЕНЕРИРУЕМ ДО ТОГО КАК ОН БУДЕТ В УКАЗАННОЙ ТОЧКЕ 
-								next =Random.Range (0, obj.Length);
-								if (next == 1) {
-										clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (1f, 3.3f) + 5f * t1, (firstY - 0.8f)), Quaternion.identity) as GameObject;
-										clone1 = Instantiate (obj [next], new Vector2 (clone1.rigidbody2D.position.x + Random.Range (1f, 3.3f) + 5f * t1, (secondY - 0.8f)), Quaternion.identity) as GameObject;
-										
-								}//ставит платформу относительно её середины
-								if (next == 0) {
-										clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (0f, 0.8f) + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
-										clone1 = Instantiate (obj [next], new Vector2 (clone1.rigidbody2D.position.x + Random.Range (0f, 0.8f) + 5f * t1, secondY - 0.8f), Quaternion.identity) as GameObject;
-										
-								}
-								if (next == 5) {
-										clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x+0.2f + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
+						if (float.IsNaN (t1)) {//должен ли он сюда заходить если условие не выполнилось <4
+								t1 = 0.3f; //костыль!!! надо убрать
+						}
+						//ЗАВИСИМОСТЬ НУЖНО ДЕЛАТЬ ОТ ПРЕДЫДУЩЕЙ ПЛАТФОРМЫ!			
+						if (clone.rigidbody2D.position.x < 4f) {			//ГЕНЕРИРУЕМ ДО ТОГО КАК ОН БУДЕТ В УКАЗАННОЙ ТОЧКЕ 
+								switch (prev) {
+								case 1:
+										next = rnd.Next (0, values.Length);
+										next = values [next];
+										if (next == 1) {
+												clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (2f, 6f) + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
+												prev = next;
+												//clone1 = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (0f, 0.8f) + 5f * t1, secondY - 0.8f), Quaternion.identity) as GameObject;
+										}
+										if (next == 0) {
+												clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (0f, 1.6f) + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
+												prev = next;
+										}
+										if (next == 2) {
+												clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (1f, 3.3f) + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
+												prev = next;
+										}
+										//clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (1f, 3.3f) + 5f * t1, (firstY - 0.8f)), Quaternion.identity) as GameObject;
+										//clone1 = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (1f, 3.3f) + 5f * t1, (secondY - 0.8f)), Quaternion.identity) as GameObject;
+										break;
+								//ставит платформу относительно её середины
+								case 0:
+										next = rnd.Next (0, values.Length);
+										next = values [next];
+										if (next == 1) {
+												clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (0f, 0.8f) + Random.Range (1f, 3.3f) + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
+												prev = next;
+												//clone1 = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (0f, 0.8f) + 5f * t1, secondY - 0.8f), Quaternion.identity) as GameObject;
+										}
+										if (next == 0) {
+												clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (0f, 1.6f) + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
+												prev = next;
+										}
+										if (next == 2) {
+												clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
+												prev = next;
+										}
+										break;
+								case 2:
+										next = rnd.Next (0, values.Length );
+										next = values [next];
+										if (next == 1) {
+												clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (1f, 3.3f) + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
+												prev = next;
+												//clone1 = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (0f, 0.8f) + 5f * t1, secondY - 0.8f), Quaternion.identity) as GameObject;
+										}
+										if (next == 0) {
+												clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (0f, 0.8f) + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
+												prev = next;
+										}
+										if (next == 2) {
+												clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + 5f * t1, firstY - 0.8f), Quaternion.identity) as GameObject;
+												prev = next;
+										}
+										break;
+								default:
+										break;
 								}
 								y0 = firstY - 0.8f;
-								sY=secondY-0.8f;
+								sY = secondY - 0.8f;
 								//ОШИБКА!!!//character.rigidbody2D.position.y-0.3f;//берет координаты в середине человека поэтому -0.3
 								firstY = Random.Range (0.8f, 4.5f); 
-								secondY=Random.Range (-4f,0f);
-								while (Mathf.Abs(firstY-y0)>funRecord){
-					//область в которой функция будет иметь вещественные корни
+								secondY = Random.Range (-4f, 0f);
+								while (Mathf.Abs(firstY-y0)>funRecord) {//БАГ!!!! нужно продумать!
+										//область в которой функция будет иметь вещественные корни
 										firstY = Random.Range (0.8f, 4.5F);
-										secondY=Random.Range (-4f,0f);
+										secondY = Random.Range (-4f, 0f);
 								}
 						}
 						//y = Random.Range (y0-2.2f, y0+2f); 
