a3eeb888fe1463715eb2199b39df40aca6890cd7
AlbertPodusenko <albert.podusenko@gmail.com>
2014-11-03 02:01:46 +0300

    нельзя делать прыжок 200! Это первая причина, по которой появляются комплексные корни! Максимум функции почти в нуле.

--- ProjectX/Assets/Scripts/SpawnScript.cs
+++ ProjectX/Assets/Scripts/SpawnScript.cs
@@ -5,7 +5,7 @@ public class SpawnScript : MonoBehaviour
 {
 public GameObject[] obj;
 public GameObject startPlatform, clone1,clone2,player;
-public float y0 = -0.4f, sY, firstY = 1f, secondY = -2f, x0 = 0, x, funRecord; //squareEquation
+public float y0 = -0.4f, sY, firstY = 1f, secondY = -2f, x0 = 0, x; //squareEquation
 public float standOfNull=0.7f, halfLengthOfGreat=2f,halfLengthOfMiddle=0.95f,halfLengthOfMini=0.5f;
 public float gravity =30f, tao=0.02f, t1,force;
 public int switchcase = 1, beforePrev=0, next, prev;
@@ -41,8 +41,7 @@ float SquareEquationSmall (float y0, float y, int force)//почему бы не
 void Start ()
 {
 		// for albert
-	obj [1].GetComponent<Gravity> ().getGravity ();
-	funRecord = 2.3f;
+	//obj [1].GetComponent<Gravity> ().getGravity ();
 	System.Random rnd = new System.Random ();
 	if (startPlatform.transform.position.x < 3.5f) {
 			t1 = SquareEquationSmall (y0, firstY,600);
@@ -53,10 +52,8 @@ void Start ()
 			
 			//clone1 = Instantiate (obj [0], new Vector2 (4 * 2f + 5f * t1, secondY - 0.8f), Quaternion.identity) as GameObject;
 			prev = next;
-			y0 = firstY - 0.8f;//character.rigidbody2D.position.y-0.3f;//берет координаты в середине человека поэтому -0.3
-			firstY = Random.Range (0.8f, 4.5f); 
-			while (firstY-y0>2.3F)//область в которой функция будет иметь вещественные корни
-					firstY = Random.Range (0F, 5F);
+			y0 = firstY - standOfNull;//character.rigidbody2D.position.y-0.3f;//берет координаты в середине человека поэтому -0.3
+			firstY = 1f;
 	}
 
 }
@@ -64,8 +61,8 @@ void LetClone (ref GameObject clone, ref float y)
 {
 		System.Random rnd = new System.Random ();
 		t1 = SquareEquationSmall (y0, y, obj [prev].GetComponent<Gravity> ().getGravity ());
-		force = obj [prev].GetComponent<Gravity> ().getGravity ();
-
+		//force = obj [prev].GetComponent<Gravity> ().getGravity ();
+		print(t1);
 		//SetGravity.setGravity(obj [prev].GetComponent<Gravity> ().getGravity ());
 		
 		if (float.IsNaN(t1))
@@ -141,11 +138,12 @@ void LetClone (ref GameObject clone, ref float y)
 			default:
 				break;
 			}
-			y0 = y - standOfNull;
+			y0 = y;
 
-				y = Random.Range (-4f, y0 + 2.3f);
-				if (y> 4.5f) {
-					y = Random.Range (0.8f, y0);
+			y = Random.Range (y0, y0);
+			y0 = y-standOfNull;
+				//if (y> 4.5f) {
+					//y = Random.Range (0.8f, y0);
 
 			/*else 
 			{
@@ -156,7 +154,7 @@ void LetClone (ref GameObject clone, ref float y)
 				if (y>-1) {
 					y--;
 				}*/
-			}
+			//}
 		}
 		
 
