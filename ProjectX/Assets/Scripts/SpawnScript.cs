c4f898bb80187f0a2bf5dd283c83fdd2560fd237
AlbertPodusenko <albert.podusenko@gmail.com>
2014-11-11 02:23:29 +0300

    discussNeed
    Короче я нашел все ошибки генерации! Потребовалось часа 2! По генерации
    дополнительных платформ - надо обсудить всё!

--- ProjectX/Assets/Scripts/SpawnScript.cs
+++ ProjectX/Assets/Scripts/SpawnScript.cs
@@ -7,24 +7,13 @@ public GameObject[] obj;
 public GameObject startPlatform, clone1,clone2,player;
 public float y0 = -0.4f, sY, firstY = 1f, secondY = -2f, x0 = 0, x; //squareEquation
 public float standOfNull=0.7f, halfLengthOfGreat=2f,halfLengthOfMiddle=0.95f,halfLengthOfMini=0.5f;
-public float gravity =30f, tao=0.02f, t1,force;
+public float gravity =30f, tao=0.02f, t1,t2,force, extremum;
 public int switchcase = 1, beforePrev=0, next, prev;
-int[] values = new int[5] {0,0,1,1,2}; //0-middle 1-greate 2-mini
+int[] values = new int[5] {1,1,1,1,1}; //0-middle 1-greate 2-mini
 public bool counter = true;
+//-gt^2/2+a*tao*t+(y0-a*tao^2/2-y)=0
 
-float SquareEquationBig (float y0, float y)//пока не работает двойной прыжок
-{
-	float t2;
-	t1 = (-2f *12f + Mathf.Sqrt (2f * 12f * 12f + 4f * 15f * (y0 - y - 2f * 0.12f))) / (-30f);
-	t2 = (-2f * 12f - Mathf.Sqrt (2f * 12f * 12f + 4f * 15f * (y0 - y - 2f * 0.12f))) / (-30f);
-	if (t1 > t2)
-			return t1;
-	else {
-			t1 = t2;
-			return t1;
-	}}
-
-	// R: Использовать goto, это плохо
+	// R: Использовать goto, это плохо //
 float SquareEquationSmall (float y0, float y, int force)//почему бы не передать y1,y2,y3 и в let clone использовать goto;
 {
 	float t2;
@@ -40,6 +29,7 @@ float SquareEquationSmall (float y0, float y, int force)//почему бы не
 
 void Start ()
 {
+	t1=0;
 		// for albert
 	//obj [1].GetComponent<Gravity> ().getGravity ();
 	System.Random rnd = new System.Random ();
@@ -48,7 +38,7 @@ void Start ()
 			next = rnd.Next (0, values.Length);
 			next = values [next];
 			clone1 = Instantiate (obj [next], new Vector2 (3.478378f * 2f + 5f * t1, firstY - standOfNull), Quaternion.identity) as GameObject;
-			//clone2 = Instantiate (obj [next], new Vector2 (3.478378f * 2f + 5f * t1, secondY - standOfNull), Quaternion.identity) as GameObject;
+			clone2 = Instantiate (obj [next], new Vector2 (3.478378f * 2f + 5f * t1, secondY - standOfNull), Quaternion.identity) as GameObject;
 			
 			//clone1 = Instantiate (obj [0], new Vector2 (4 * 2f + 5f * t1, secondY - 0.8f), Quaternion.identity) as GameObject;
 			prev = next;
@@ -57,7 +47,7 @@ void Start ()
 	}
 
 }
-void LetClone (ref GameObject clone, ref float y)
+void LetClone (ref GameObject clone,ref GameObject clone1, ref float y)
 {
 		System.Random rnd = new System.Random ();
 		t1 = SquareEquationSmall (y0, y, obj [prev].GetComponent<Gravity> ().getGravity ());
@@ -65,8 +55,11 @@ void LetClone (ref GameObject clone, ref float y)
 		print(t1);
 		//SetGravity.setGravity(obj [prev].GetComponent<Gravity> ().getGravity ());
 		
-		//if (float.IsNaN(t1))
-			//t1=0.4f;
+		if (float.IsNaN (t1)) {
+						
+
+						t1 = 0.4f;
+				}
 		//ЗАВИСИМОСТЬ НУЖНО ДЕЛАТЬ ОТ ПРЕДЫДУЩЕЙ ПЛАТФОРМЫ!			
 		if (clone.rigidbody2D.position.x < 4f) {			//ГЕНЕРИРУЕМ ДО ТОГО КАК ОН БУДЕТ В УКАЗАННОЙ ТОЧКЕ 
 			// TODO: FOR Albert: почему-то ставится гравитация следующей платформы, а не текущей
@@ -77,21 +70,26 @@ void LetClone (ref GameObject clone, ref float y)
 				next = values [next];
 				if (next == 1) {
 					clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x+ Random.Range(halfLengthOfGreat,2*halfLengthOfGreat) + 5f * t1, y - standOfNull), Quaternion.identity) as GameObject; //5-скорость платформы
-					//prev = next;
-					//clone1 = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (0f, 0.8f) + 5f * t1, secondY - 0.8f), Quaternion.identity) as GameObject;
+					clone1 = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x+ Random.Range(halfLengthOfGreat,2*halfLengthOfGreat) + 5f * t1, sY - standOfNull), Quaternion.identity) as GameObject; //5-скорость платформы
 				}
 				if (next == 0) {
 					clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (halfLengthOfGreat, halfLengthOfGreat+halfLengthOfMiddle) + 5f * t1, y - standOfNull), Quaternion.identity) as GameObject;
+					clone1 = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (halfLengthOfGreat, halfLengthOfGreat+halfLengthOfMiddle) + 5f * t1, sY - standOfNull), Quaternion.identity) as GameObject;
+					
 					//prev = next;
 				}
 				if (next == 2) {
 					clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (halfLengthOfGreat, halfLengthOfGreat+halfLengthOfMini) + 5f * t1, y - standOfNull), Quaternion.identity) as GameObject;
+					clone1 = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (halfLengthOfGreat, halfLengthOfGreat+halfLengthOfMini) + 5f * t1, sY - standOfNull), Quaternion.identity) as GameObject;
+					
 					//prev = next;
 				}
 				// TODO: FOR Albert: почему-то ставится гравитация следующей платформы, а не текущей
 
 
 				prev=next;
+				extremum=2.3f;
+				
 				//clone = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (1f, 3.3f) + 5f * t1, (firstY - 0.8f)), Quaternion.identity) as GameObject;
 				//clone1 = Instantiate (obj [next], new Vector2 (clone.rigidbody2D.position.x + Random.Range (1f, 3.3f) + 5f * t1, (secondY - 0.8f)), Quaternion.identity) as GameObject;
 				break;
@@ -114,6 +112,8 @@ void LetClone (ref GameObject clone, ref float y)
 				}
 				// TODO: FOR Albert: почему-то ставится гравитация следующей платформы, а не текущей
 				prev = next;
+				extremum=1.5f;
+				
 				break;
 			case 2:
 				next = rnd.Next (0, values.Length);
@@ -134,13 +134,16 @@ void LetClone (ref GameObject clone, ref float y)
 				// TODO: FOR Albert: почему-то ставится гравитация следующей платформы, а не текущей
 
 				prev=next;
+				extremum=0.3f;
 				break;
 			default:
 				break;
 			}
-			y0 = y;
+			y0 = y-standOfNull;
 
-			y = Random.Range (y0, y0);
+			y = Random.Range (-1f, y0+extremum);//ВОТ ЕБАНАЯ ОШИБКА!!! В УРАВНЕНИИ НЕТ ОШИБОК! (-1f,y0+extr) y0 может стать тупо меньше первого аргумента
+			sY=y-Random.Range (2f,3f);
+			t2 = SquareEquationSmall (y0, sY, obj [prev].GetComponent<Gravity> ().getGravity ());
 			//y0 = y-standOfNull;
 				//if (y> 4.5f) {
 					//y = Random.Range (0.8f, y0);
@@ -161,8 +164,8 @@ void LetClone (ref GameObject clone, ref float y)
 }
 void Update ()
 {
-		LetClone (ref clone1, ref firstY);
-		//LetClone (ref clone2, ref secondY);
+		LetClone (ref clone1,ref clone2, ref firstY);
+		//Главное ведь то, что есть платформа, на которую можно запрыгнуть, остальные можно делать рандомно
 }
 
 }
