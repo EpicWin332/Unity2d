3659d37107b65de1a049d0027596dc678173d2cc
AlbertPodusenko <albert.podusenko@gmail.com>
2014-12-29 19:51:18 +0300

    sounds

--- ProjectX/Assets/Scripts/DestroyerScript.cs
+++ ProjectX/Assets/Scripts/DestroyerScript.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class DestroyerScript : MonoBehaviour 
 {
+	public AudioClip death;
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Player") 
 		{
+			audio.clip=death;
+			audio.Play();
 			CameraScript.dead=true;
 		}
 		if ((other.gameObject.transform.parent) && (other.tag != "Player"))
 		{
+
 			Destroy (other.gameObject.transform.parent.gameObject);
 		}
 		else
